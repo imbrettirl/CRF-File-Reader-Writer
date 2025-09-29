fb78e6a50086529e52f188595b5f1825da9fbc08
imbretty <114277284+imbrettirl@users.noreply.github.com>
2025-09-29 13:45:57 -0600

    Writer
    -updated writer

--- crf_writer.py
+++ crf_writer.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass # for easy structuring https://docs.python.org/3/library/dataclasses.html 
 from typing import List # imported for readability https://docs.python.org/3/library/typing.html
 
+import struct # transition from plain-text to binary
+
 @dataclass
 class Account: # Type of account + Balance of that account
     name: str
@@ -18,15 +20,21 @@ class CreditRecord: # All info on the person and their accounts
 
 class CreditReportWriter:
     @staticmethod
+    def write_string(f, s: str):
+        encoded = s.encode("utf-8") # converts to bytes
+        f.write(struct.pack("<I", len(encoded))) # converts length into 4-byte unsigned integers & records length
+        f.write(encoded) # writes UTF-8 encoded string
+
+    @staticmethod
     def write_file(filename: str, records: List[CreditRecord]):
         with open(filename, "wb") as f: # write in binary
             for record in records:
-                f.write(record.sin.encode() + b"\n")
-                f.write(record.name.encode() + b"\n")
-                f.write(record.address.encode() + b"\n")
-                f.write(str(record.credit_score).encode() + b"\n")
-                f.write(str(record.account_count).encode() + b"\n")
-                f.write(str(record.major_flags).encode() + b"\n")
+                CreditReportWriter.write_string(f, record.sin)
+                CreditReportWriter.write_string(f, record.name)
+                CreditReportWriter.write_string(f, record.address)
+                f.write(struct.pack("<I", record.credit_score))
+                f.write(struct.pack("<I", record.account_count))
+                f.write(struct.pack("<I", record.major_flags))
                 for acc in record.accounts:
-                    f.write(acc.name.encode() + b"\n")
-                    f.write(str(acc.balance).encode() + b"\n")
+                    CreditReportWriter.write_string(f, acc.name)
+                    f.write(struct.pack("<i", acc.balance)) # signed 4-byte integer
